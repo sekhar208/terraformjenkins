f9e55fd15c3f13e566066c328824dfad0456f440
Chandra <oneplus208@gmail.com>
2021-06-08 11:01:10 +0530

    Added TF files for testing from jenkins

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "aws_region" {}
+variable "amis" {
+    description = "AMIs by region"
+    default = {
+        us-east-1 = "ami-0747bdcabd34c712a" # ubuntu 18.04 LTS
+	us-east-2 = "ami-0b9064170e32bde34" # ubuntu 18.04 LTS
+    }
+}
+variable "vpc_cidr" {}
+variable "vpc_name" {}
+variable "IGW_name" {}
+variable "key_name" {}
+variable "public_subnet1_cidr" {}
+variable "public_subnet2_cidr" {}
+variable "public_subnet3_cidr" {}
+variable "private_subnet_cidr" {}
+variable "public_subnet1_name" {}
+variable "public_subnet2_name" {}
+variable "public_subnet3_name" {}
+variable "private_subnet_name" {}
+variable Main_Routing_Table {}
+variable "azs" {
+  description = "Run the EC2 Instances in these Availability Zones"
+  type = "list"
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+variable "environment" { default = "dev" }
+variable "instance_type" {
+  type = "map"
+  default = {
+    dev = "t2.nano"
+    test = "t2.micro"
+    prod = "t2.medium"
+    }
+}
+
